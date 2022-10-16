978d85dbb906f13eefa5e6b85151da525a7f847b
AdityaPatil1603 <115254565+AdityaPatil1603@users.noreply.github.com>
2022-10-16 10:34:19 +0530

    Update Fibonacci series.py

--- Fibonacci series.py
+++ Fibonacci series.py
@@ -2,12 +2,9 @@ num=50
 a=0
 b=1
 sum=0
-if num<=0:
-    print("invalid input ")
-else:
-    for i in range(0,num):
-         print(sum,end=" ")
-         a=b
-         b=sum
-         sum=a+b
+for i in range(0,num):
+    print(sum,end=" ")
+    a=b
+    b=sum
+    sum=a+b
    
