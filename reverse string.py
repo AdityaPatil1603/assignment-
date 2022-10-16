815514aa5ebca04c5e51c1bec558ea9869cb7e0d
AdityaPatil1603 <115254565+AdityaPatil1603@users.noreply.github.com>
2022-10-16 11:44:55 +0530

    Update reverse string.py

--- reverse string.py
+++ reverse string.py
@@ -1,6 +1,11 @@
-x=input("enter the word")
+x=input("enter the word:")
 y=''
 for char in x:
     y= char + y
 print(x)
-print(y
+print(y)
+
+#expected output:
+enter the word:edyoda
+edyoda
+adoyde
