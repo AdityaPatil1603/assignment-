79103590625f5f7b5444d5e2d1f81a8526b38c76
AdityaPatil1603 <115254565+AdityaPatil1603@users.noreply.github.com>
2022-10-24 13:07:15 +0530

    Add files via upload

---
+++ sort list in increasing order.py
@@ -0,0 +1,12 @@
+def sort_tuple(tup):
+    x=len(tup)
+    for i in range(0,x):
+        for j in range (0 , x-i-1):
+            if (tup[j][-1] > tup[j+1][-1]):
+                temp=tup[j]
+                tup[j]=tup[j+1]
+                tup[j+1]=temp
+    return tup
+tup=[(2,5),(1,2),(4,4),(2,3),(2,1)]
+print(tup)
+print(sort_tuple(tup))
