1c99fb97e22d87f9a1726de0fc503628089e6c0e
AdityaPatil1603 <115254565+AdityaPatil1603@users.noreply.github.com>
2022-11-06 18:30:31 +0530

    Add files via upload

---
+++ addition using lambda .py
@@ -0,0 +1,2 @@
+y= lambda x,y:x+y
+print("addition of 10 and 25 is :",y(10,25))
---
+++ square of the element using lambda.py
@@ -0,0 +1,5 @@
+def square(x):
+    return x**2
+x=[4, 5, 2, 9]
+l=list(map(square,x))
+print("The output of list is :",l)
